cf4f44cc927950bd50f6d4c358ddea3561e1250a
albertomontilla17 <montillalberto@icloud.com>
2021-08-12 23:48:37 -0400

    Working with GeoJSON

---
+++ Mapping_GeoJSON_Points/static/js/logic2.js
@@ -0,0 +1,25 @@
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport).addTo(map)
